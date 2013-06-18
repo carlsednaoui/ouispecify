9818fa5134e85eb0ecb5dfb764d3c4b84f8f176e
carl sednaoui <carlsednaoui@gmail.com>
2013-06-18 14:20:11 -0400

    Rename files, remove unused code

--- example.js
+++ example.js
@@ -1,13 +1,4 @@
-document.getElementById('specify').onsubmit = function() {
-  updateSpecificity();
-  return false;
-};
-
 window.onkeyup = function() {
-  updateSpecificity();
-};
-
-function updateSpecificity() {
   cssSelector = document.getElementById('css-selectors').value;
   document.getElementById('result').innerHTML = specify(cssSelector);
-}
+};
