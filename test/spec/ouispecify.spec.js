2fec7248c8529b7401ec19829af1123b3105d93c
carl sednaoui <carlsednaoui@gmail.com>
2013-06-19 16:50:15 -0400

    Refactor tests

--- test/spec/ouispecify.spec.js
+++ test/spec/ouispecify.spec.js
@@ -2,20 +2,20 @@ describe("Ouispecify", function() {
   it("exports a specify function", function() {
     expect(specify).not.toBe(null);
   });
+});
 
+describe("This library", function() {
   it("finds the specificity of 'h1'", function() {
     var expectedResult = [0,0,1];
     expect(specify('h1').specificity).toEqual(expectedResult);
   });
 
-  it("finds the specificity of 'h1#id.class'", function() {
-    var expectedResult = [1,1,1];
-    expect(specify('h1#id.class').specificity).toEqual(expectedResult);
-  });
+  it("finds the specificity & details of 'h1#id.class'", function() {
+    var expectedSpecificity = [1,1,1];
+    var expectedDetails = [['#id'],['.class'],['h1']];
 
-  it("finds the details of 'h1#id.class'", function() {
-    var expectedResult = [['#id'],['.class'],['h1']];
-    expect(specify('h1#id.class').details).toEqual(expectedResult);
+    expect(specify('h1#id.class').specificity).toEqual(expectedSpecificity);
+    expect(specify('h1#id.class').details).toEqual(expectedDetails);
   });
 
   it("finds the details of 'input[type=text]'", function() {
@@ -43,34 +43,27 @@ describe("Ouispecify", function() {
     expect(specify('h1 + h2').details).toEqual(expectedResult);
   });
 
-  it("finds the details of the :not selector on ':not(p)'", function() {
-    var expectedResult = [[],[],['p']];
-    expect(specify(':not(p)').details).toEqual(expectedResult);
+  it("finds the specificity & details of the :not selector on ':not(p)'", function() {
+    var expectedSpecificity = [0,0,1];
+    var expectedDetails = [[],[],['p']];
+
+    expect(specify(':not(p)').specificity).toEqual(expectedSpecificity);
+    expect(specify(':not(p)').details).toEqual(expectedDetails);
   });
 
-  it("finds the specificity of the :not selector on ':not(p)'", function() {
-    var expectedResult = [0,0,1];
-    expect(specify(':not(p)').specificity).toEqual(expectedResult);
+  it("finds the specificity & details of the pseudo-elements on 'p::first-letter'", function() {
+    var expectedSpecificity = [0,0,2];
+    var expectedDetails = [[],[],['p', '::first-letter']];
+
+    expect(specify('p::first-letter').specificity).toEqual(expectedSpecificity);
+    expect(specify('p::first-letter').details).toEqual(expectedDetails);
   });
 
-  it("finds the specificity of the pseudo-elements on 'p::first-letter'", function() {
-    var expectedResult = [0,0,2];
-    expect(specify('p::first-letter').specificity).toEqual(expectedResult);
-  });
+  it("finds the specificity & details of 'p:after'", function() {
+    var expectedSpecificity = [0,0,2];
+    var expectedDetails = [[],[],['p', ':after']];
 
-  it("finds the details of the pseudo-elements on 'p::first-letter'", function() {
-    var expectedResult = [[],[],['p', '::first-letter']];
-    expect(specify('p::first-letter').details).toEqual(expectedResult);
+    expect(specify('p:after').specificity).toEqual(expectedSpecificity);
+    expect(specify('p:after').details).toEqual(expectedDetails);
   });
-
-  it("finds the specificity of 'p:after'", function() {
-    var expectedResult = [0,0,2];
-    expect(specify('p:after').specificity).toEqual(expectedResult);
-  });
-
-  it("finds the details of 'p:after'", function() {
-    var expectedResult = [[],[],['p', ':after']];
-    expect(specify('p:after').details).toEqual(expectedResult);
-  });
-
 });
